7d57e1b1e9c79372ecc93f92f5cc3550f4fef5a7
Lhyerin <113421398+Lhyerin@users.noreply.github.com>
2022-09-13 16:57:36 +0900

    modification

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
-	printf("hello sookmyung Electronics\n");
+	int a,b;
+	printf("%d+%d=%d\n",a,b,a+b);
 	return 0;
 }
+
